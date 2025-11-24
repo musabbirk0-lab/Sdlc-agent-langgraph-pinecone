efc43e5a782a510e770c251f36be996d0b0ba0dc
Musabbir Ahmed Khan <musabbirk0@gmail.com>
2025-11-24 06:01:37 -0800

    Update sdlc_agent_dag.py

--- airflow/dags/sdlc_agent_dag.py
+++ airflow/dags/sdlc_agent_dag.py
@@ -1,1 +1,23 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import requests
 
+
+DEFAULT_ARGS = {
+'owner': 'airflow',
+'depends_on_past': False,
+'retries': 0,
+}
+
+
+with DAG('sdlc_agent_dag', start_date=datetime(2025,1,1), schedule_interval='@daily', default_args=DEFAULT_ARGS, catchup=False) as dag:
+def trigger_agent(**kwargs):
+resp = requests.post('http://sdlc-agent:8000/run', json={'task':'create_test_cases','context':'daily-run'})
+print(resp.json())
+
+
+run_task = PythonOperator(task_id='trigger_agent', python_callable=trigger_agent)
+
+
+run_task
